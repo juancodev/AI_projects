51a7345d8d78d270c43e881907d8d7be7545a610
juancodev <montillasanchezjuancarlos@gmail.com>
2024-10-14 20:03:12 -0400

    🚧 Practice django app

--- image_to_text/urls.py
+++ image_to_text/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import hello
-from .views import about
+from .views import hello, about, user, projects, tasks, project_with_id
 
 
 urlpatterns = [
     path("", hello),
     path("about/", about),
+    path("user/<str:username>", user),
+    path("projects/", projects),
+    path("projects/<int:id>", project_with_id),
+    path("tasks/", tasks)
 ]
--- image_to_text/views.py
+++ image_to_text/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+# metodo que me permite responder un status code de 404 y no detener el servidor
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpRequest, JsonResponse
+from .models import Project
+from .models import Task
 
 
 # Create your views here.
@@ -9,3 +12,27 @@ def hello(request):
 
 def about(request):
     return HttpResponse("<h1>About</h1>")
+
+def user(request, username):
+    # Esta es la manera de obtener parámetros desde la URL
+    print(username)
+    # forma de concatenar un valor desde el método HttpResponse
+    return HttpResponse("<h1>Hello %s</h1>" % username)
+
+def projects(request):
+    # De esta manera vamos a recibir un valor de tipo conjunto y no podemos responder eso del lado del cliente para eso necesitamos usar un formato JsonResponse.
+
+    # Para obtener solamente los datos y no el modelo completo, lo hacemos enviando el método .values()
+    projects = list(Project.objects.values())
+    return JsonResponse(projects, safe=False)
+
+def project_with_id(request, id):
+    result = Project.objects.get(id=id)
+    # Esto me sirve para hacer la consulta sin errores
+    # primer argumento: el modelo y segundo argumento: la consulta
+    result = get_object_or_404(Project, id=id)
+    return HttpResponse("<h1>Projects: %s</h1>" % result.name)
+
+def tasks(request: HttpRequest):
+    print(request)
+    return HttpResponse("<h1>Tasks</h1>")
